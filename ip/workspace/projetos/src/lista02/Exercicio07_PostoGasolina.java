7a0131316e3f47af79684f5712057496e706af28
Carlos Marin <carlosgmarin749@gmail.com>
2026-03-12 20:09:01 -0300

    Exercicio Combustivel

--- ip/workspace/projetos/src/lista02/Exercicio07_PostoGasolina.java
+++ ip/workspace/projetos/src/lista02/Exercicio07_PostoGasolina.java
@@ -7,62 +7,54 @@ public class Exercicio07_PostoGasolina {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		DecimalFormat formatador = new DecimalFormat("0.00");
 		Scanner teclado = new Scanner(System.in);
-		
+
 		System.out.print("Digite o combustível abastecido com A para Ácool e G para Gasolina: ");
 		String combustivel = teclado.nextLine();
-				
+
 		System.out.print("Digite quantos litros foram abastecidos: ");
-		double litrosAlcool = teclado.nextDouble();
-		double litrosGasolina = teclado.nextDouble();
-		
-		double gasolina = 6.90;
+		double litrosAbastecidos = teclado.nextDouble();
+
 		double alcool = 6.20;
+		double gasolina = 6.90;
 		
-		if (combustivel == "A") {
-		} if (litrosAlcool <= 20) {
-			System.out.println("Total a pagar: R$ "
-					+ formatador.format((litrosAlcool * alcool) * 0.97));
-		} else {
-			if (combustivel == "A") {
-		}	 	if (litrosAlcool > 20) {
-					System.out.println("Total a pagar: R$ "
-					+ formatador.format((litrosAlcool * alcool) * 0.95));
-		} else {
-		} 	if (combustivel == "G") {
-			} 	if (litrosGasolina <= 20) {
-					System.out.println("Total a pagar: R$ "
-						+ formatador.format((litrosGasolina * gasolina) * 0.96));		
-		} else {
-			if (combustivel == "G") {
-			}	if (litrosGasolina > 20) {
-					System.out.println("Total a pagar: R$ "
-					+ formatador.format((litrosGasolina * gasolina) * 0.94));
-		
-//		} else {
-//			System.out.println("Digite um combustível válido!");
+		switch(combustivel) {
+			case "A": { 
+				if (litrosAbastecidos <= 20) {
+					System.out.println("Total a pagar: R$ " + formatador.format((litrosAbastecidos * alcool) * 0.97));
+				} else {
+					System.out.println("Total a pagar: R$ " + formatador.format((litrosAbastecidos * alcool) * 0.95));
+				}
+				break;
+			}
+			case "G": {
+				if (litrosAbastecidos <= 20) {
+					System.out.println("Total a pagar: R$ " + formatador.format((litrosAbastecidos * gasolina) * 0.96));
+				} else {
+					System.out.println("Total a pagar: R$ " + formatador.format((litrosAbastecidos * gasolina) * 0.94));
+				}
+				break;
+			}
 		}
 		
 		teclado.close();
 		
 		/*
-		 * UM POSTO DE COMBUSTÍVEL ESTÁ VENDENDO SEUS COMBUSTÍVEIS COM A SEGUINTE TABELA DE DESCONTOS:
+		 * UM POSTO DE COMBUSTÍVEL ESTÁ VENDENDO SEUS COMBUSTÍVEIS COM A SEGUINTE TABELA
+		 * DE DESCONTOS:
 		 * 
-		 * ÁLCOOL 		ATÉ 20 LITROS, DESCONTO DE 3% POR LITRO
-		 * 				ACIMA DE 20 LITROS, DESCONTO DE 5% POR LITRO
+		 * ÁLCOOL ATÉ 20 LITROS, DESCONTO DE 3% POR LITRO ACIMA DE 20 LITROS, DESCONTO
+		 * DE 5% POR LITRO
 		 * 
-		 * GASOLINA 	ATÉ 20 LITROS, DESCONTO DE 4% POR LITRO
-		 * 				ACIMA DE 20 LITROS, DESCONTO DE 6% POR LITRO
+		 * GASOLINA ATÉ 20 LITROS, DESCONTO DE 4% POR LITRO ACIMA DE 20 LITROS, DESCONTO
+		 * DE 6% POR LITRO
 		 * 
-		 * DESENVOLVA UM PROGRAMA QUE LEIA O NÚMERO DE LITROS VENDIDOS E O TIPO DE COMBUSTÍVEL
-		 * (CODIFICADO DA SEGUINTE FORMA: A -> ÁLCOOL, G -> GASOLINA), CALCULE E IMPRIMA O VALOR A SER PAGO
-		 * PELO CLIENTE SABENDO-SE QUE O PREÇO DO LITRO DA GASOLINA É R$ 6,90 E O PREÇO DO LITRO DO ÁLCOOL É R$ 6,20.
+		 * DESENVOLVA UM PROGRAMA QUE LEIA O NÚMERO DE LITROS VENDIDOS E O TIPO DE
+		 * COMBUSTÍVEL (CODIFICADO DA SEGUINTE FORMA: A -> ÁLCOOL, G -> GASOLINA),
+		 * CALCULE E IMPRIMA O VALOR A SER PAGO PELO CLIENTE SABENDO-SE QUE O PREÇO DO
+		 * LITRO DA GASOLINA É R$ 6,90 E O PREÇO DO LITRO DO ÁLCOOL É R$ 6,20.
 		 */
-
-				}
-				
-				}
-		}
-		}
+	}
+}
